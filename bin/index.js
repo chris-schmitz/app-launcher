c333a0daaaaf9725f6a967e7a3303a657f05903f
chris-schmitz <schmitz.chris@gmail.com>
2016-11-09 08:44:57 -0600

    Electron updates for webpack. Adds in launch process communication.

--- bin/index.js
+++ bin/index.js
@@ -2,23 +2,13 @@
 
 const program = require('commander')
 const chalk = require('chalk')
-const {exec} = require('child_process')
-const path = require('path')
-const config = require('../config')
+const {getPromiseArrayForGroup} = require('../lib/launchGroup')
 
 
 program
     .option('-l, --location <location>', 'The apps you want to open per a specific location (work, home, etc)')
     .parse(process.argv)
 
-let applist = config[program.location]
-
-if(!applist) throw new Error(chalk.red('Invalid location argument.'))
-
-applist.forEach((appPath) => {
-    exec(`open "${appPath}"`, (err,stdout,stderr) => {
-        if(err) throw new Error(chalk.red(err))
-        if(stderr) throw new Error(chalk.red(stderr))
-        console.log(`Opened application: ${ chalk.blue( path.basename(appPath) ) }`)
-    })
-})
+Promise.all(getPromiseArrayForGroup(program.location))
+    .then(result => console.log(chalk.blue(result.join('\n'))))
+    .catch(result => console.log(chalk.red(result)))
